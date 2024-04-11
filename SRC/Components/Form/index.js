5cb81e36ba3d6c48fdb18abcc85eacac3177f6c9
nahbaratella <natybaratella05@gmail.com>
2024-04-11 10:49:57 -0300

    Formulario 3B1 lado B

---
+++ SRC/Components/Form/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { TextInput } from "react-native-web";
+
+export default function Form() {
+    return (
+        <View>
+
+            <View>
+                <Text>Altura</Text>
+                <TextInput
+                placeholder="Ex.1.75"
+                keyboardType="numeric"
+                />
+
+                <Text>Peso</Text>
+                <TextInput
+                placeholder="Ex. 85.766"
+                keyboardType="numeric"
+                />
+
+            </View>
+
+        </View>
+    );
+}
--- SRC/Components/Main/index.js
+++ SRC/Components/Main/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, Text } from "react-native";
 
-export default function Title() {
+export default function Main() {
     return (
         <View>
             <Text>Aplicativo IMC</Text>
